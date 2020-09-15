297442d504517e139cebb840f30de38bc692e4aa
arbrown110 <arbrown110@gmail.com>
2020-09-15 17:37:53 -0400

    edited model, controller,and views.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -11,12 +11,16 @@ before_action :redirect_if_not_signed_in
  end
 
  def new
+  if params[:school_id] && @school = School.find_by_id(params[:school_id])
+   @playlists = @schools.playlist.build
    @playlist = Playlist.new
    @playlist.songs.build(artist: "1", tune: "1")
    @playlist.songs.build(artist: "2", tune: "2")
    @playlist.songs.build(artist: "3", tune: "3")
    @playlist.songs.build(artist: "4", tune: "4")
    @playlist.songs.build(artist: "5", tune: "5")
+  else
+    render :playlist
  end
   
  def create
@@ -30,19 +34,36 @@ before_action :redirect_if_not_signed_in
  end
 
  def show
-   @playlist = Playlist.find(params[:id])
+  collect_playlist
  end
 
  def edit
-  @playlist = Playlist.find(params[:id])
+  collect_playlist
  end
 
+ def update
+  if collect_playlist
+    redirect_to playlist_path(@playlist)
+  else 
+    render :edit
+  end
+ end
+
+
  def delete
-  @playlist = Playlist.find(params[:id])
+  collect_playlist
+  @playllist.destroy
+  redirect_to playlists_path
  end
 
 
  private
+
+ def collect_playlist
+  @playlist = Playlist.find(params[:id])
+  if @playlist
+ end
+
 
  def playlist_params
   params.require(:playlist).permit(:user_id, :title, :date, :ratings, songs_attributes: [ :artist, :tune ])
